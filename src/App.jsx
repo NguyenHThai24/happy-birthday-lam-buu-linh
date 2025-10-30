4a485684111d42859e513b3a4a920bc0f64d3676
Ngh Tahi <thaingh2402@gmail.com>
2025-10-30 08:23:42 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     if (stage === "fallingLetters") {
       const timer = setTimeout(() => {
         setStage("gallery");
-      }, 93000);
+      }, 85000);
       return () => clearTimeout(timer);
     }
   }, [stage]);
